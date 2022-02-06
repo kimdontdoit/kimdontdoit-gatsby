ddfd1675ca1fdc96008b627eb92d0b010d106895
Vladislav Kim <vkim@o2web.ca>
2022-02-06 00:55:56 -0500

    Move hook from theme

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -1,17 +1,36 @@
-import React, { useEffect, useState } from "react";
-import useCursorPosition from "@kimdontdoit/the-great-gatsby-theme/src/hooks/useCursorPosition";
+import React, { useState, useEffect } from "react";
+
 import "./Cursor.scss";
 
 const Cursor = (props) => {
-  const { mouseX, mouseY } = useCursorPosition();
   const { image } = props;
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    addEventListeners();
+    return () => removeEventListeners();
+  });
+
+  const addEventListeners = () => {
+    document.addEventListener("mousemove", onMouseMove);
+  };
+
+  const removeEventListeners = () => {
+    document.removeEventListener("mousemove", onMouseMove);
+  };
+
+  const onMouseMove = (e) => {
+    console.log(e.clientX + ", " + e.clientY);
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
 
   return (
     <div
       className="cursor fixed"
       style={{
-        left: `${mouseX}px`,
-        top: `${mouseY}px`,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
       }}
     >
       {image && (
