e0356fd8ad73a04d6d94ad7ba5daedf459c05adf
Vladislav Kim <vladkimkim@gmail.com>
2022-11-13 21:13:23 -0500

    multilingual sitemap

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { graphql } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import { useI18next } from 'gatsby-plugin-react-i18next'
