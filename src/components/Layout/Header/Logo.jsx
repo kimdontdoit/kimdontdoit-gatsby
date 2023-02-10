2fbf93e2353b7c26b5eba66e2310e09fafcff4eb
Vladislav Kim <vlad@kimdontdoit.com>
2023-02-09 23:26:54 -0500

    remove logo tooltip on home

--- src/components/Layout/Header/Logo.jsx
+++ src/components/Layout/Header/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "gatsby-plugin-react-i18next";
 import Tooltip from "@mui/material/Tooltip";
+import { useLocation } from "@reach/router";
 import classNames from "the-great-gatsby-theme/src/utils/classNames";
 
 import logoDark from "../../../images/kimdontdoit-logo-2011.png";
@@ -23,23 +24,30 @@ const toolTipTwitter = {
 };
 
 export const Logo = ({ sticky, light }) => {
+  const { pathname } = useLocation();
+
+  const logo = (
+    <img
+      alt="Kimdontdoit WAVY logo"
+      src={light ? logoLight : logoDark}
+      height="69"
+      width="151"
+      className={classNames(classes.root, sticky && classes.sticky)}
+    />
+  );
+
+  if (pathname === "/" || pathname === "/en/") {
+    return logo;
+  }
+
   return (
     <Tooltip
       id="logo-tooltip"
       title="Back to home"
       arrow
       componentsProps={toolTipTwitter}
-      enterDelay
     >
-      <Link to="/">
-        <img
-          alt="Kimdontdoit WAVY logo"
-          src={light ? logoLight : logoDark}
-          height="69"
-          width="151"
-          className={classNames(classes.root, sticky && classes.sticky)}
-        />
-      </Link>
+      <Link to="/">{logo}</Link>
     </Tooltip>
   );
 };
