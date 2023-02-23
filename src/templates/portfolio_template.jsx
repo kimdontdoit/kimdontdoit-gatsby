76f6ae60b9eccbafe997db12190aa6366f3bdcf8
Vladislav Kim <vlad@kimdontdoit.com>
2023-02-23 16:19:47 -0500

    experimental - grid scroll

--- src/templates/portfolio_template.jsx
+++ src/templates/portfolio_template.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useContext } from "react";
 import { graphql } from "gatsby";
 import { useI18next } from "gatsby-plugin-react-i18next";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import Pageheader from "the-great-gatsby-theme/src/components/Pageheader";
 import Seo from "the-great-gatsby-theme/src/components/Seo";
 import ThemeContext from "../context/ThemeContext";
-
 import { PortfolioPost } from "../components/Post";
+
+import "./portfolio.css";
+gsap.registerPlugin(ScrollTrigger);
 
 export default function PortfolioTemplate({ data }) {
   const { t } = useI18next("index");
@@ -19,34 +22,96 @@ export default function PortfolioTemplate({ data }) {
   }, []);
 
   const { type } = data;
-  const posts = data.posts.nodes;
+  // const posts = data.posts.nodes;
 
-  const crumbs = [];
+  const gridItems = [...document.querySelectorAll(".grid > .grid__item")];
 
-  crumbs.push({
-    label: t("back-to-home"),
-    url: "/"
+  gridItems.forEach((item) => {
+    const image = item.querySelector(".grid__item-img");
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: item,
+          start: "top top",
+          end: "bottom top",
+          scrub: true
+        }
+      })
+      .set(image, {
+        transformOrigin: `${gsap.utils.random(0, 1) > 0.5 ? 0 : 100}% 100%`
+      })
+      .to(image, {
+        ease: "none",
+        scale: 0
+      });
+  });
+
+  const posts = [
+    "/media/wow-vladislav-kim.jpeg",
+    "/media/wow-vladislav-kim.jpeg",
+    "/media/wow-vladislav-kim.jpeg",
+    "/media/wow-vladislav-kim.jpeg",
+    "/media/wow-vladislav-kim.jpeg"
+  ];
+
+  let rows = [];
+  let columns = [];
+  let column;
+  let row;
+
+  const portfolioContent = posts.map((post, index) => {
+    while (!columns.includes(column)) {
+      column = Math.floor(Math.random() * 8) + 1;
+      if (!columns.includes(column)) columns.push(column);
+    }
+
+    row = index + 1;
+    while (!rows.includes(row)) {
+      if (!rows.includes(row)) rows.push(row);
+    }
+
+    return (
+      <div
+        className={`grid__item`}
+        style={{ "--c": column, "--r": row }}
+        key={index}
+      >
+        <div
+          className="grid__item-img"
+          style={{
+            backgroundImage: `url(/media/wow-vladislav-kim.jpeg)`
+          }}
+        >
+          {row},{column}
+        </div>
+      </div>
+    );
   });
 
   return (
     <>
       <Seo title={type.frontmatter.title} />
       <div>
-        <section className={`my-24 container`}>
-          <h1>{type.frontmatter.title}</h1>
-        </section>
-
         <section className={`container pb-16`}>
-          <div className="max-w-screen-lg mx-auto">
-            {posts &&
-              posts.map((post) => {
-                return (
-                  <PortfolioPost
-                    key={post.id}
-                    node={post.childMarkdownRemark}
-                  />
-                );
-              })}
+          {/*<svg width="18px" height="18px" viewBox="0 0 24 24">
+                  <path d="M18.25 15.5a.75.75 0 00.75-.75v-9a.75.75 0 00-.75-.75h-9a.75.75 0 000 1.5h7.19L6.22 16.72a.75.75 0 101.06 1.06L17.5 7.56v7.19c0 .414.336.75.75.75z"></path>
+  </svg>*/}
+
+          <div className="content">
+            <div className="grid">{portfolioContent}</div>
+
+            <div className="cover">
+              <h1 className="cover__title font-display">Vladislav Kim</h1>
+              <h3 className="cover__subtitle">並外れたファッション</h3>
+            </div>
+
+            <div className="footer">
+              <p>
+                Manifesto: programming is such an interesting turn in our
+                history.
+              </p>
+            </div>
           </div>
         </section>
       </div>
